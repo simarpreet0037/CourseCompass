93473b86b33ec5e8e57495a505d06ab6a19a28bb
simarpreet0037 <157557572+simarpreet0037@users.noreply.github.com>
2026-02-12 14:23:31 -0600

    Remove static files collection from entrypoint
    Removed the static files collection step from the entrypoint.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,8 +4,5 @@ set -e
 echo "Running database migrations..."
 python manage.py migrate --noinput
 
-echo "Collecting static files..."
-python manage.py collectstatic --noinput
-
 echo "Starting application..."
 exec "$@"
