1c5da259a0a2d591663c8fbe079fca2a4d36e696
Simarpreet Purba <simarpreet.0037@gmail.com>
2026-02-13 12:44:02 -0600

    Refactor prod settings to be env-driven for HTTP/HTTPS deployments

--- CourseCompass/settings/prod.py
+++ CourseCompass/settings/prod.py
@@ -7,19 +7,23 @@ from .base import *
 DEBUG = False
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS",default=["localhost", "127.0.0.1"])
+USE_HTTPS = env.bool("USE_HTTPS", default=False)
+CSRF_TRUSTED_ORIGINS = env.list("CSRF_TRUSTED_ORIGINS",default=[])
 
 # Security settings for production
 SECURE_BROWSER_XSS_FILTER = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
 X_FRAME_OPTIONS = 'DENY'
-CSRF_COOKIE_SECURE = True
-SESSION_COOKIE_SECURE = True
 
-# HTTPS settings (uncomment when using HTTPS)
-# SECURE_SSL_REDIRECT = True
-# SECURE_HSTS_SECONDS = 31536000
-# SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-# SECURE_HSTS_PRELOAD = True
+# HTTPS settings
+CSRF_COOKIE_SECURE = USE_HTTPS
+SESSION_COOKIE_SECURE = USE_HTTPS
+
+if USE_HTTPS:
+    SECURE_SSL_REDIRECT = True
+    SECURE_HSTS_SECONDS = 31536000
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = True
 
 # Production logging - less verbose
 LOGGING['root']['level'] = 'WARNING'
