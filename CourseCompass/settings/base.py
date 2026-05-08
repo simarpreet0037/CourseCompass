af0afa29031ceeba42c5f4c3e6c52297f50427dd
Simarpreet Purba <simarpreet.0037@gmail.com>
2026-05-08 12:54:35 -0600

    Refactor code structure for improved readability and maintainability and made changes to UI

--- CourseCompass/settings/base.py
+++ CourseCompass/settings/base.py
@@ -84,7 +84,8 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
